f85e541238f7f61b5d58ac318b76cee225e66851
ilya.viache <ilya.viache@gmail.com>
2023-05-16 07:53:46 +0700

    testflight

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -14,6 +14,24 @@ export class TelegramService {
   private client: TelegramClient;
 
   constructor() {
+    function checkWordsInText(text, words) {
+      // Преобразуем строку текста в нижний регистр для сравнения без учета регистра
+      const lowercaseText = text.toLowerCase();
+
+      // Проверяем наличие каждого слова в тексте
+      for (const word of words) {
+        // Преобразуем слово в нижний регистр для сравнения без учета регистра
+        const lowercaseWord = word.toLowerCase();
+
+        // Ищем слово в тексте
+        if (lowercaseText.includes(lowercaseWord)) {
+          return true; // Если найдено хотя бы одно слово, возвращаем true
+        }
+      }
+
+      return false; // Если ни одно слово не найдено, возвращаем false
+    }
+
     const apiId = 26775843;
     const apiHash = '35f1cfd51cde86ffe6a656ecf6cffb06';
     const stringSession = new StringSession(sessionString); // fill this later with the value from session.save()
@@ -31,13 +49,13 @@ export class TelegramService {
       console.log('You should now be connected.');
       console.log(client.session.save()); // Save this string to avoid logging in again
 
-      const result = await client.invoke(
-        new Api.contacts.ResolveUsername({
-          username: 'ilyaviache',
-        })
-      );
-      console.log(result);
-      const userId = result.users[0].id;
+      // const result = await client.invoke(
+      //   new Api.contacts.ResolveUsername({
+      //     username: 'ilyaviache',
+      //   })
+      // );
+      // console.log(result);
+      // const userId = result.users[0].id;
 
       // const sendResult = await client.sendMessage(userId, {
       //   message: 'test 123',
@@ -45,47 +63,66 @@ export class TelegramService {
       // console.log('sendResult', sendResult);
       async function eventPrint(event: NewMessageEvent) {
         const message = event.message;
-
-        const chanellId = message.peerId['channelId'];
-        console.log('chanellId', chanellId);
-        // sender & message data
-        const senderId = message.senderId;
         const text = message.text;
-        const messageId = message.id;
 
-        const result = await client.invoke(
-          new Api.channels.GetFullChannel({
-            channel: chanellId,
-          })
-        );
-        const channelData = result.chats[0];
+        if (
+          checkWordsInText(text, [
+            'bike',
+            'байк',
+            'скутер',
+            'мотоцикл',
+            'мопед',
+            'мот',
+            'pcx',
+            'nmax',
+            'xmax',
+            'forza',
+            'click',
+            'нмакс',
+            'дрон',
+            'drone',
+          ])
+        ) {
+          const chanellId = message.peerId['channelId'];
 
-        const user = await client.invoke(
-          new Api.users.GetFullUser({
-            id: senderId,
-          })
-        );
+          // sender & message data
+          const senderId = message.senderId;
+          const messageId = message.id;
 
-        const channelName = channelData['title'];
-        const channelUsername = channelData['username'];
+          const result = await client.invoke(
+            new Api.channels.GetFullChannel({
+              channel: chanellId,
+            })
+          );
+          const channelData = result.chats[0];
 
-        console.log('user', user.users);
-        const senderUsername = user.users[0]['username'];
+          const user = await client.invoke(
+            new Api.users.GetFullUser({
+              id: senderId,
+            })
+          );
 
-        const report = `
+          const channelName = channelData['title'];
+          const channelUsername = channelData['username'];
+
+          const senderUsername = user.users[0]['username'];
+
+          const report = `
           Username: @${senderUsername}\n
           Channel: ${channelName} @${channelUsername}\n
           Message: ${text}\n
         `;
 
-        const to = 'test_booooottaaaaaaa';
+          const to = 'test_booooottaaaaaaa';
 
-        await client.sendMessage(to, { message: report });
-        await client.forwardMessages(to, {
-          fromPeer: message.peerId,
-          messages: messageId,
-        });
+          await client.sendMessage(to, { message: report });
+          await client.forwardMessages(to, {
+            fromPeer: message.peerId,
+            messages: messageId,
+          });
+        }
       }
+
       // adds an event handler for new messages
       client.addEventHandler(eventPrint, new NewMessage({}));
     })();
