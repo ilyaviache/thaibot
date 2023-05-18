64c8e1fcc0ba21173114e700dbc093d624beab5c
ilya.viache <ilya.viache@gmail.com>
2023-05-18 11:56:12 +0700

    add redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
@@ -13,5 +13,10 @@ export class AppController {
   @Get('hello/:name')
   getHelloName(@Param('name') name: string): string {
     return this.appService.getHelloName(name);
+  }
+
+  @Get('herbal')
+  getHerbal(@Res() res) {
+    return res.redirect('https://www.instagram.com/herb.haze/');
   }
 }
